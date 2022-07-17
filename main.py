a262b0d5775c79349ba9bf890fd137801cb50ad4
vedantshah99 <vedantshah@ucsb.edu>
2022-07-17 00:30:16 -0700

    attempt at functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ast import expr_context
+from lib2to3.pgen2 import driver
 from xml.dom.minidom import Element
 from selenium import webdriver
 from selenium.webdriver import Firefox
@@ -17,176 +18,184 @@ import requests
 import csv
 import re
 
-
-#driver setup
-chrome_driver = 'C:\webdrivers\chromedriver.exe'
-profile_path = r'C:\Users\aayur\AppData\Roaming\Mozilla\Firefox\Profiles\tc9vu9fu.default'
-options=Options()
-options.set_preference('profile', profile_path)
-service = Service(r'C:\webdrivers\geckodriver.exe')
+companies = []
+counts = []
+variances = []
 failed = []
 
-#driver = Firefox(service=service, options=options)
-driver = webdriver.Firefox(service=service, options=options)
-driver.get('https://www.sec.gov/edgar/search/#/q=covid&dateRange=custom&startdt=2017-06-26&enddt=2020-05-27')
+class Scrape():
+    #driver setup
+    def driverSetup(self):
+        chrome_driver = 'C:\webdrivers\chromedriver.exe'
+        profile_path = r'C:\Users\aayur\AppData\Roaming\Mozilla\Firefox\Profiles\tc9vu9fu.default'
+        options=Options()
+        options.set_preference('profile', profile_path)
+        service = Service(r'C:\webdrivers\geckodriver.exe')
 
-def variance(data):
-    n = len(data)
-    mean = sum(data) / n
-    deviations = [(x - mean) ** 2 for x in data]
-    variance = sum(deviations) / n
-    return variance
+        self.driver = webdriver.Firefox(service=service, options=options)
+        self.driver.get('https://www.sec.gov/edgar/search/#/q=covid&dateRange=custom&startdt=2017-06-26&enddt=2020-05-27')
 
-def findVar(ticker, startDate, endDate):
-    driver.execute_script(f"window.open('https://finance.yahoo.com/quote/{ticker}/history?p={ticker}', 'new_window')")
-    
-    driver.switch_to.window(driver.window_handles[1])
+    #calculate variance
+    def variance(data):
+        n = len(data)
+        mean = sum(data) / n
+        deviations = [(x - mean) ** 2 for x in data]
+        variance = sum(deviations) / n
+        return variance
 
-    #choose dates button
-    time.sleep(3)
-    try:
-        dates = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/div[1]/div/div/div/span')))
-        driver.execute_script("window.scrollTo(0, (document.body.scrollHeight)/2)")
-        dates.click()
-    except TimeoutException:
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        print("Timeout Exception for: " + ticker)
-        failed.append("Timeout Exception for: " + ticker)
-        return 0
-    except NoSuchElementException:
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        print("Dates not found for: " + ticker)
-        failed.append("Dates not found for: " + ticker)
-        return 0
-    time.sleep(3)
+    #find variance
+    def findVar(self, ticker, startDate, endDate):
+        #open yahoo finance
+        self.driver.execute_script(f"window.open('https://finance.yahoo.com/quote/{ticker}/history?p={ticker}', 'new_window')")
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        time.sleep(3)
 
-    #choosing the start and end dates
-    try:
-        initial = driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[1]/input')
-        initial.send_keys(Keys.ENTER)
-        startDateReader = driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/input')
-        ActionChains(driver).move_to_element(startDateReader).send_keys(startDate).perform()
-        startDateReader.send_keys('02/02/2002')
-        endDateReader = driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/input')
-        ActionChains(driver).move_to_element(endDateReader).send_keys(endDate).perform()
-        time.sleep(2)
-    except:
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        print("Start & End Dates not found for: " + ticker)
-        failed.append("Start & End Dates not found for: " + ticker)
-        return 0
-    #endDateReader.send_keys(endDate)
-
-    #click on done button
-    try:
-        done = driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[3]/button[1]')
-        ActionChains(driver).move_to_element(done).click().perform()
-        time.sleep(2)
-    except NoSuchElementException:
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        print("Done Button not found for: " + ticker)
-        failed.append("Done Button not found for: " + ticker)
-        return 0
-
-    #click on apply button
-    apply = driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/button')
-    apply.click()
-    time.sleep(2)
-
-    #collecting data
-    data = []
-    i = 1
-    while i < 10:
-        xpath = '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr['+str(i)+']/td[5]/span'
+        #choose dates button
         try:
-            input = driver.find_element(By.XPATH, xpath).text
+            dates = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/div[1]/div/div/div/span')))
+            self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight)/2)")
+            dates.click()
+        except TimeoutException:
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            print("Timeout Exception for: " + ticker)
+            self.failed.append("Timeout Exception for: " + ticker)
+            return 0
         except NoSuchElementException:
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            print("Dates not found for: " + ticker)
+            self.failed.append("Dates not found for: " + ticker)
+            return 0
+        time.sleep(3)
+
+        #choosing the start and end dates
+        try:
+            initial = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[1]/input')
+            initial.send_keys(Keys.ENTER)
+            startDateReader = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/input')
+            ActionChains(self.driver).move_to_element(startDateReader).send_keys(startDate).perform()
+            startDateReader.send_keys('02/02/2002')
+            endDateReader = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[2]/input')
+            ActionChains(self.driver).move_to_element(endDateReader).send_keys(endDate).perform()
+            time.sleep(2)
+        except:
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            print("Start & End Dates not found for: " + ticker)
+            self.failed.append("Start & End Dates not found for: " + ticker)
+            return 0
+
+        #click on done button
+        try:
+            done = self.driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/div[3]/button[1]')
+            ActionChains(self.driver).move_to_element(done).click().perform()
+            time.sleep(2)
+        except NoSuchElementException:
+            self.driver.close()
+            self.driver.switch_to.window(driver.window_handles[0])
+            print("Done Button not found for: " + ticker)
+            self.failed.append("Done Button not found for: " + ticker)
+            return 0
+
+        #click on apply button
+        apply = self.driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/button')
+        apply.click()
+        time.sleep(2)
+
+        #collecting data
+        data = []
+        i = 1
+        while i < 10:
+            xpath = '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr['+str(i)+']/td[5]/span'
+            try:
+                input = self.driver.find_element(By.XPATH, xpath).text
+            except NoSuchElementException:
+                i+=1
+                continue
+            data.append(float(input.replace(",","")))
             i+=1
-            continue
-        data.append(float(input.replace(",","")))
-        i+=1
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+        self.driver.close()
+        self.driver.switch_to.window(driver.window_handles[0])
 
-    return variance(data)
+        return self.variance(data)
 
-searchbox = driver.find_element(by=By.ID, value='entity-short-form')
-searchbox.send_keys('covid')
-searchbox.send_keys(Keys.RETURN)
+    def navigateSEC(self):
+        searchbox = self.driver.find_element(by=By.ID, value='entity-short-form')
+        searchbox.send_keys('covid')
+        searchbox.send_keys(Keys.RETURN)
 
-button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'headingTwo2')))
-button.click()
+        button = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'headingTwo2')))
+        button.click()
 
-tenq = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div/table/tbody/tr[2]/td/a')))
-tenq.click()
-time.sleep(2)
+        tenq = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div/table/tbody/tr[2]/td/a')))
+        tenq.click()
+        time.sleep(2)
 
-docs = WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, "preview-file"))
-)
+        self.docs = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "preview-file"))
+        )
 
-#adding company names to list
-companies = []
-variances = []
-temps = driver.find_elements(By.CLASS_NAME, "entity-name")
-variances.append(findVar('KBRS', '03/13/2020', '05/27/2020'))
-variances.append(findVar('AAN', '03/13/2020', '05/27/2020'))
-variances.append(findVar('PRO', '03/13/2020', '05/27/2020'))
-for i in range(1,len(temps)):
-    print(i)
-    t = temps[i].text
-    if t.find("(") >= 0:
-        tick = t[t.find("(")+1:t.find(")")]
-        tick = tick[tick.find(" ")+1:len(tick)]
-        companies.append(tick)
-        variances.append(findVar(tick, '03/13/2020', '05/27/2020'))
-    else:
-        print("i = " + str(i) + " and t = " + str(t) + ", len(temps) = "+ str(len(temps)) +", and len(docs) = "+ str(len(docs)))
+    def addData(self):
+        #adding tickers and varainces to lists
+        listOfCompanyNames = self.driver.find_elements(By.CLASS_NAME, "entity-name")
+        for i in range(1,len(listOfCompanyNames)):
+            print(i)
+            fullCompanyName = listOfCompanyNames[i].text
+            # find ticker symbol and pass it into findVar
+            if fullCompanyName.find("(") >= 0:
+                tick = fullCompanyName[fullCompanyName.find("(")+1:fullCompanyName.find(")")]
+                tick = tick[tick.find(" ")+1:len(tick)]
+                companies.append(tick)
+                variances.append(self.findVar(tick, '03/13/2020', '05/27/2020'))
+            else:
+                self.docs.remove(self.docs[i])
 
-        docs.remove(docs[i])
+    def addCovidCounts(self):
+        #adding covid counts to list
+        print("docs: " + str(len(self.docs)))
+        print("companies: " + str(len(companies)))
+        for i in range(len(self.docs)):
+            #click on each 10Q form
+            self.docs[i].click();
+            time.sleep(2)
 
-with open('fails.csv','w') as file:
+            #count number of times COVID was mentioned
+            try:
+                count = self.driver.find_element(By.CLASS_NAME, "find-counter").text
+                count = count[5:len(count)]
+                counts.append(int(count))
+            except:
+                close = self.driver.find_element(By.CLASS_NAME, "close")
+                close.click()
+                counts.append(-1)
+                continue
+
+            time.sleep(1)
+
+            #close the preview
+            close = self.driver.find_element(By.CLASS_NAME, "close")
+            close.click()
+
+def writeFailsToCSV(self):
+    with open('fails.csv','w') as file:
+            writer = csv.writer(file)
+            for val in failed:
+                writer.writerow(val)
+
+def writeToCSV(self):
+    with open('list.csv','w') as file:
         writer = csv.writer(file)
-        for val in failed:
-            writer.writerow(val)
+        writer.writerow(companies)
+        writer.writerow(counts)
+        writer.writerow(variances)
 
-#adding covid counts to list
-i = 0
-counts = []
+if __name__ == "__main__":
+    scrape = Scrape()
+    scrape.driverSetup()
+    scrape.navigateSEC()
+    scrape.addData()
+    scrape.addCovidCounts()
+    scrape.writeFailsToCSV()
 
-print("docs: " + str(len(docs)))
-print("temps: " + str(len(temps)))
-print("companies: " + str(len(companies)))
-while i < len(docs):
-    #click on each 10Q form
-    docs[i].click();
-    time.sleep(2)
-    print(docs[i])
-    #count number of times COVID was mentioned
-    try:
-        count = driver.find_element(By.CLASS_NAME, "find-counter").text
-        count = count[5:len(count)]
-        counts.append(int(count))
-    except:
-        close = driver.find_element(By.CLASS_NAME, "close")
-        close.click()
-        i = i+1
-        continue
-
-    time.sleep(1)
-
-    #close the preview
-    close = driver.find_element(By.CLASS_NAME, "close")
-    close.click()
-
-    i = i+1
-
-with open('list.csv','w') as file:
-    writer = csv.writer(file)
-    writer.writerow(companies)
-    writer.writerow(counts)
-    writer.writerow(variances)
+    writeToCSV()
--- testing.py
+++ testing.py
@@ -1,4 +1,10 @@
-str = "balls, green"
-fixed = str.replace(",", "")
+import csv
 
-print(fixed)
+list1 = [1,2,3,4,5,6]
+list2 = ["red", "orange", "yellow", "green", "blue", "purple"]
+list3 = ['a', 'b', 'c', 'd', 'e', 'f']
+
+with open('test.csv','w', newline='') as file:
+    writer = csv.writer(file)
+    for i in range(len(list1)):
+        writer.writerow([str(list1[i])] + [list2[i]] + [list3[i]])
